e068280def2d592efffad16e31b8f00efefdc19f
iftekhar <iftekharislamchowdhury@gmail.com>
2023-11-02 13:42:29 -0400

    fixed single userdata api

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, render_template
 import sqlite3
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
+CORS(app)
 
 def get_db_connection():
     conn = None 
@@ -24,8 +26,12 @@ def userdata():
         user_info = [dict(id=row[0], time=row[1], title=row[2], user_data=row[3], user_id=row[4])
                      for row in cursor.fetchall()
                      ]
+        
         if user_info is not None:
             return jsonify(user_info)
+        user_info.headers.add('Access-Control-Allow-Origin', '*')
+    
+        
         
     if request.method == "POST":
 
@@ -45,14 +51,16 @@ def userinfo():
     cursor = conn.cursor()
     
     if request.method== "GET":
-            cursor = conn.execute("select * from userinfo")
-            userdata = [dict(user_id = row[0], user_name=row[1], time=row[2])
+        cursor = conn.execute("select * from userinfo")
+        userdata = [dict(user_id = row[0], user_name=row[1], time=row[2])
                         for row in cursor.fetchall()
-                        ]
+                    ]
             
-            if userdata is not None:
-                return jsonify(userdata)
+        if userdata is not None:
+            return jsonify(userdata)
+        
     
+        
     if request.method == "POST":
         
         user_id = request.form['user_id']
@@ -66,8 +74,46 @@ def userinfo():
         
         
         
-        
+@app.route("/userdata/<int:id>", methods = ["GET", "PUT", "DELETE"])
+
+def single_user_data(id):
     
-    conn.close()
-    # return jsonify(read_user_data)
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    userdata = None 
+    
+    if request.method == "GET":
+        
+        cursor.execute("SELECT * FROM chattitleinfo where id=?", (id,))
+        
+        userdata = [dict(id = row[0], time=row[1], title=row[2], user_data=row[3], user_id = row[4])
+                        for row in cursor.fetchall()
+                    ]
+       
+        if userdata is not None: 
+            return jsonify(userdata), 200
+        else:
+            return "Something wrong", 404
+    
+    
+    if request.method == "PUT":
+        sql = """ UPDATE chattitleinfo SET id=?, title=?, user_data=?, user_id=? """
+        title = request.form["title"]
+        userdata = request.form["user_data"]
+        user_id = request.form["user_id"]
+        
+        updated_userdata = {"id": id, "title": title, "user_data": userdata, "user_id":user_id,}
+        
+        conn.execute(sql,( title,userdata, user_id))
+        conn.commit()
+        return jsonify(updated_userdata)
+    
+    
+    if request.method == "DELETE":
+        
+        sql = """ DELETE FROM chattitleinfo WHERE id=? """
+        conn.execute(sql,(id,))
+        conn.commit()
+        return "The user chat information with the id: {} has been deleted.".format(id), 200
+
     
